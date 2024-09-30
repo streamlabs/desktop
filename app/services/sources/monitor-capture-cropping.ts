344d5bd5dd565cb0c88afa20a48c8fc5f748024f
Akihiko Koizuka <koizuka@gmail.com>
2024-09-30 14:57:38 +0900

    monitor-capture-cropping: monitor_id 仕様変更に追従

--- app/services/sources/monitor-capture-cropping.ts
+++ app/services/sources/monitor-capture-cropping.ts
@@ -1,11 +1,12 @@
 import electron, { BrowserWindow } from 'electron';
 import { Inject } from 'services/core/injector';
 import { WindowsService } from 'services/windows';
-import { ISourcesServiceApi } from 'services/sources';
+import { ISourceApi, ISourcesServiceApi } from 'services/sources';
 import { StatefulService, mutation } from '../core/stateful-service';
 import { ScalableRectangle, ResizeBoxPoint } from 'util/ScalableRectangle';
 import { SceneItem } from '../scenes';
 import * as remote from '@electron/remote';
+import * as Sentry from '@sentry/vue';
 
 interface IMonitorCaptureCroppingServiceState {
   sceneId: string | null;
@@ -59,11 +60,8 @@ export class MonitorCaptureCroppingService extends StatefulService<IMonitorCaptu
     }
 
     const source = this.sourcesService.getSource(sourceId);
-    const targetDisplayId = source.getSettings().monitor;
-    const displays = remote.screen.getAllDisplays();
-    const display = displays[targetDisplayId];
+    const display = getDisplayFromSource(source, 'startCropping');
     if (!display) {
-      console.error('クロップ対象のソースが指定するモニタが存在しません');
       return;
     }
 
@@ -111,11 +109,8 @@ export class MonitorCaptureCroppingService extends StatefulService<IMonitorCaptu
     const rect = new ScalableRectangle(sceneItem.getRectangle());
 
     const source = sceneItem.getSource();
-    const targetDisplayId = source.getSettings().monitor;
-    const displays = remote.screen.getAllDisplays();
-    const display = displays[targetDisplayId];
+    const display = getDisplayFromSource(source, 'crop');
     if (!display) {
-      console.error('クロップ対象のソースが指定するモニタが存在しません');
       return;
     }
 
@@ -160,4 +155,71 @@ export class MonitorCaptureCroppingService extends StatefulService<IMonitorCaptu
   CLEAR_WINDOW_ID() {
     this.state.windowId = null;
   }
+}
+
+function getDisplayFromSource(source: ISourceApi, label: string): Electron.Display | null {
+  const monitorId = source.getSettings().monitor_id;
+  const propMonitors = (
+    source.getPropertiesFormData().find(prop => prop.name === 'monitor_id') as {
+      options: { value: string; description: string }[];
+    }
+  )?.options;
+
+  const monitorIndex = propMonitors.findIndex(monitor => monitor.value === monitorId);
+  // Autoを除いた番号に変換し、Autoのときはその先頭の値にする
+  // Autoを除いた順序はElectron, OBSともに Windows API の EnumDisplayMonitors の列挙順に従うため、一致すると想定する
+  const defaultMonitorIndex = propMonitors[0].value === 'Auto' ? 1 : 0;
+  const targetDisplayId =
+    monitorIndex < defaultMonitorIndex ? 0 : monitorIndex - defaultMonitorIndex;
+
+  const displays = remote.screen.getAllDisplays();
+
+  if (targetDisplayId >= displays.length) {
+    Sentry.captureMessage('getDisplayFromSource: 対象のdisplay IDが範囲外です', {
+      level: 'error',
+      tags: {
+        label,
+        propMonitors: propMonitors.length,
+        displays: displays.length,
+      },
+      extra: {
+        monitorId,
+        propMonitors,
+        targetDisplayId,
+        displays: displaysForSentry(displays),
+      },
+    });
+    return null;
+  }
+
+  if (displays.length !== 1 || propMonitors.length !== 2) {
+    // モニタが複数あるときは選択が正しそうなのかを確認するためにログを残す
+    Sentry.captureMessage('getDisplayFromSource: multiple monitors', {
+      level: 'info',
+      tags: {
+        label,
+        propMonitors: propMonitors.length,
+        displays: displays.length,
+      },
+      extra: {
+        monitorId,
+        propMonitors,
+        targetDisplayId,
+        displays: displaysForSentry(displays),
+      },
+    });
+  }
+
+  return displays[targetDisplayId];
+}
+
+function displaysForSentry(displays: Electron.Display[]): any {
+  // Sentry の extra 内でネストされた内側のオブジェクトは [Object] に省略されて見えなくなるので、文字列化する
+  return displays.map(display => ({
+    ...display,
+    bounds: JSON.stringify(display.bounds),
+    size: JSON.stringify(display.size),
+    workArea: JSON.stringify(display.workArea),
+    workAreaSize: JSON.stringify(display.workAreaSize),
+  }));
 }
