7b12fc9957e2d62886c7ce03437537ffbef25896
asaday <asada@nagisaworks.com>
2024-12-19 10:55:54 +0900

    cancel error処理

--- app/services/nicolive-program/speech/VoicevoxSynthesizer.ts
+++ app/services/nicolive-program/speech/VoicevoxSynthesizer.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/vue';
 import { Speech } from '../nicolive-comment-synthesizer';
 import { ISpeechSynthesizer } from './ISpeechSynthesizer';
 
@@ -8,34 +9,55 @@ export class VoicevoxSynthesizer implements ISpeechSynthesizer {
   private speakingResolve: () => void | null = null;
   private speakingCounter: number = 0;
 
-  async output(speech: Speech) {
-    try {
-      const id = speech.voicevox?.id ?? '1';
-      // POSTだがqueryで
-      const r1 = await fetch(
-        `${VoicevoxURL}/audio_query?speaker=${id}&text=${encodeURIComponent(speech.text)}`,
-        { method: 'POST' },
-      );
+  private audio?: HTMLAudioElement = null;
+  private canceled = false;
 
-      const r2 = await r1.json();
-      r2.speedScale = speech.rate;
-      r2.volumeScale = speech.volume;
+  async playAudioAndWait(blob: Blob): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.audio?.pause();
 
-      const r3 = await fetch(`${VoicevoxURL}/synthesis?speaker=${id}`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', Accept: 'audio/wav' },
-        body: JSON.stringify(r2),
-      });
-
-      const r4 = await r3.blob();
-      const url = URL.createObjectURL(r4);
-
+      const url = URL.createObjectURL(blob);
       const audio = new Audio(url);
-      await audio.play();
-      URL.revokeObjectURL(url);
-    } catch (e) {
-      console.error(e);
-    }
+      this.audio = audio;
+
+      const done = () => {
+        audio.src = '';
+        this.audio = null;
+        URL.revokeObjectURL(url);
+        resolve();
+      };
+
+      audio.addEventListener('pause', done, { once: true });
+      audio.addEventListener('ended', done, { once: true });
+
+      audio.play().catch(error => reject(error));
+    });
+  }
+
+  async output(speech: Speech) {
+    this.canceled = false;
+    const id = speech.voicevox?.id ?? '1';
+    // POSTだがqueryで
+    const r1 = await fetch(
+      `${VoicevoxURL}/audio_query?speaker=${id}&text=${encodeURIComponent(speech.text)}`,
+      { method: 'POST' },
+    );
+    if (this.canceled) return;
+
+    const r2 = await r1.json();
+    if (this.canceled) return;
+    r2.speedScale = speech.rate;
+    r2.volumeScale = speech.volume;
+
+    const r3 = await fetch(`${VoicevoxURL}/synthesis?speaker=${id}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', Accept: 'audio/wav' },
+      body: JSON.stringify(r2),
+    });
+
+    const r4 = await r3.blob();
+    if (this.canceled) return;
+    await this.playAudioAndWait(r4);
   }
 
   speakText(speech: Speech, onstart: () => void, onend: () => void) {
@@ -50,19 +72,33 @@ export class VoicevoxSynthesizer implements ISpeechSynthesizer {
       }
 
       onstart();
-      this.output(speech).then(() => {
-        if (--this.speakingCounter === 0) {
-          this.speakingResolve();
-          this.speakingPromise = null;
-          this.speakingResolve = null;
-        }
-        onend();
-      });
+      this.output(speech)
+        .catch(error => {
+          Sentry.withScope(scope => {
+            scope.setLevel('error');
+            scope.setTag('in', 'VoicevoxSynthesizer:speakText');
+            scope.setExtra('speech', speech);
+            scope.setExtra('error', error);
+            scope.setFingerprint(['VoicevoxSynthesizer', 'speakText', 'error']);
+            Sentry.captureException(error);
+          });
+          console.info(`VoicevoxSynthesizer: text:${JSON.stringify(speech.text)} -> ${error}`);
+        })
+        .finally(() => {
+          if (--this.speakingCounter === 0) {
+            this.speakingResolve();
+            this.speakingPromise = null;
+            this.speakingResolve = null;
+          }
+          onend();
+        });
 
       this.speakingCounter++;
       return {
         cancel: async () => {
-          speechSynthesis.cancel();
+          this.audio?.pause();
+          this.audio = null;
+          this.canceled = true;
         },
         running: this.speakingPromise,
       };
