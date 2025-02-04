d5e3af66147bf02993189158d3fd28a43bd30278
asaday <asada@nagisaworks.com>
2025-02-04 17:16:43 +0900

    コメント削除

--- app/components/CommentSettings.vue.ts
+++ app/components/CommentSettings.vue.ts
@@ -106,11 +106,7 @@ export default class CommentSettings extends Vue {
     this.nicoliveCommentSynthesizerService.rate = v;
   }
   get rateCandidates(): number[] {
-    return [
-      //0.1, 0.2,
-      0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.5, 1.75, 2, 3,
-      // 4, 5, 6, 7, 8, 9, 10,
-    ];
+    return [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.5, 1.75, 2, 3];
   }
   get rateDefault(): number {
     return NicoliveCommentSynthesizerService.initialState.rate;
