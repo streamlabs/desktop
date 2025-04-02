4001014549e9e6511fd448455a2b3d62209ffd39
Aleksandr Voitenko <avoitenko@logitech.com>
2025-04-02 20:43:40 +0100

    Reworked patching code

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -17,34 +17,40 @@ fs.writeFileSync(antdLibSettingsPath, JSON.stringify(antdlibSettings, null, 2));
 
 
 // The code below is needed to provide a better uninstall experience.
-// Here we patch assistedInstaller.nsh to avoid redudant uninstaller welcome page.
+// Here we patch assistedInstaller.nsh to avoid redundant uninstaller welcome page.
 // When we upgrade electron-builder package to at least 23.0.6 we need to use the removeDefaultUninstallWelcomePage
 // option and the code below can be removed.
 // @see https://stackoverflow.com/questions/73454796/default-unwelcome-page-with-electron-builder-nsis-cant-be-changed-or-removed
 const assistedInstallerPath = path.resolve('./node_modules/app-builder-lib/templates/nsis/assistedInstaller.nsh');
 
-// Read the file, modify it, and write back
+// Function to read, modify, and write without closing the file
 const commentLineInFile = (assistedInstallerPath, lineToComment, commentString = ';') => {
-  if (!fs.existsSync(assistedInstallerPath)) {
-    console.error(`File not found: ${assistedInstallerPath}`);
-    process.exit(1);
-  }
+  let fd;
 
-  let fileContent = fs.readFileSync(assistedInstallerPath, 'utf-8');
-  const modifiedContent = fileContent
-    .split('\n')
-    .map((line) => {
+  try {
+    fd = fs.openSync(assistedInstallerPath, 'r+');
+
+    const fileContent = fs.readFileSync(fd, 'utf-8');
+    const modifiedContent = fileContent
+      .split('\n')
+      .map((line) => {
         if (line.includes(lineToComment) && !line.trim().startsWith(';')) {
-            return `${commentString}${line}`;
+          return `${commentString}${line}`; // Add commentString to the target line
         }
         return line; // Leave all other lines unchanged
-    })
-    .join('\n'); // Rejoin the lines back together into a single string
+      })
+      .join('\n'); // Rejoin the lines back together into a single string
 
+    fs.ftruncateSync(fd); // Truncate the file to ensure old content is removed
+    fs.writeSync(fd, modifiedContent);
 
-  if (fs.existsSync(assistedInstallerPath)) {
-    fs.writeFileSync(assistedInstallerPath, modifiedContent, 'utf-8');
     console.log(`Post install. Successfully commented "${lineToComment}" in file: ${assistedInstallerPath}`);
+  } catch (error) {
+    console.error(`Post install. An error occurred while processing the file: ${error.message}`);
+  } finally {
+    if (fd !== undefined) {
+      fs.closeSync(fd);
+    }
   }
 };
 
