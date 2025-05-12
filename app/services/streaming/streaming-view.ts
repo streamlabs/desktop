638cf69f56d3c2effabc0999b08a9ba7bae8c7f5
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2025-05-12 17:07:11 -0400

    Revert fix streaming view type errors.

--- app/services/streaming/streaming-view.ts
+++ app/services/streaming/streaming-view.ts
@@ -321,8 +321,8 @@ export class StreamInfoView<T extends Object> extends ViewHandler<T> {
    */
   get savedSettings(): IGoLiveSettings {
     const destinations = {} as IGoLiveSettings['platforms'];
-    this.linkedPlatforms.forEach((platform: TPlatform) => {
-      destinations[platform] = this.getSavedPlatformSettings(platform);
+    this.linkedPlatforms.forEach(platform => {
+      destinations[platform as string] = this.getSavedPlatformSettings(platform);
     });
 
     // if user recently added a new platform then it doesn't have default title and description
@@ -379,22 +379,18 @@ export class StreamInfoView<T extends Object> extends ViewHandler<T> {
     // search fields in platforms that don't use custom settings first
     destinationsWithCommonSettings.forEach(platform => {
       const destSettings = getDefined(platforms[platform]);
-      Object.keys(commonFields).forEach((fieldName: keyof typeof commonFields) => {
-        if (commonFields[fieldName] || !(destSettings as Record<string, any>)[fieldName]) return;
-        commonFields[fieldName] = (destSettings as Record<keyof typeof commonFields, string>)[
-          fieldName
-        ];
+      Object.keys(commonFields).forEach(fieldName => {
+        if (commonFields[fieldName] || !destSettings[fieldName]) return;
+        commonFields[fieldName] = destSettings[fieldName];
       });
     });
 
     // search fields in platforms that have custom fields
     destinationWithCustomSettings.forEach(platform => {
       const destSettings = getDefined(platforms[platform]);
-      Object.keys(commonFields).forEach((fieldName: keyof typeof commonFields) => {
-        if (commonFields[fieldName] || !(destSettings as Record<string, any>)[fieldName]) return;
-        commonFields[fieldName] = (destSettings as Record<keyof typeof commonFields, string>)[
-          fieldName
-        ];
+      Object.keys(commonFields).forEach(fieldName => {
+        if (commonFields[fieldName] || !destSettings[fieldName]) return;
+        commonFields[fieldName] = destSettings[fieldName];
       });
     });
     return commonFields;
@@ -404,9 +400,9 @@ export class StreamInfoView<T extends Object> extends ViewHandler<T> {
     const commonFields = this.getCommonFields(platforms);
     const result = {} as IGoLiveSettings['platforms'];
     Object.keys(platforms).forEach(platform => {
-      result[platform] = platforms[platform as TPlatform];
-      result[platform].title = platforms[platform as TPlatform].title || commonFields.title;
-      result[platform].description = platforms[platform]?.description || commonFields.description;
+      result[platform] = platforms[platform];
+      result[platform].title = platforms[platform].title || commonFields.title;
+      result[platform].description = platforms[platform].description || commonFields.description;
     });
     return result;
   }
@@ -514,30 +510,25 @@ export class StreamInfoView<T extends Object> extends ViewHandler<T> {
   private getSavedPlatformSettings(platform: TPlatform) {
     const service = getPlatformService(platform);
     const savedDestinations = this.streamSettingsView.state.goLiveSettings?.platforms;
-    const { enabled, useCustomFields } = (savedDestinations &&
-      savedDestinations[platform as keyof typeof savedDestinations]) || {
+    const { enabled, useCustomFields } = (savedDestinations && savedDestinations[platform]) || {
       enabled: false,
       useCustomFields: false,
     };
     const settings = cloneDeep(service.state.settings);
 
     // don't reuse broadcastId and thumbnail for Youtube
-    if (settings && settings['broadcastId' as keyof typeof settings]) {
-      settings['broadcastId' as keyof typeof settings] = '';
-    }
-    if (settings && settings['thumbnail' as keyof typeof settings]) {
-      settings['thumbnail' as keyof typeof settings] = '';
-    }
+    if (settings && settings['broadcastId']) settings['broadcastId'] = '';
+    if (settings && settings['thumbnail']) settings['thumbnail'] = '';
 
     // don't reuse liveVideoId for Facebook
-    if (platform === 'facebook' && settings && settings['liveVideoId' as keyof typeof settings]) {
-      settings['liveVideoId' as keyof typeof settings] = '';
+    if (platform === 'facebook' && settings && settings['liveVideoId']) {
+      settings['liveVideoId'] = '';
     }
 
     // make sure platforms assigned to the vertical display in dual output mode still go live in single output mode
     const display =
       this.isDualOutputMode && savedDestinations
-        ? savedDestinations[platform as keyof typeof savedDestinations]?.display
+        ? savedDestinations[platform]?.display
         : 'horizontal';
 
     return {
