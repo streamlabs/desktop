f78dcc11a89663854dabccf983888c7b75d56c69
ggolda <38298010+ggolda@users.noreply.github.com>
2025-06-04 14:15:22 -0600

    Highlighter/remove feature flag (#5431)
    * attempt to see if it improves the amount of user who has access to highlighter and it is indeed a bug on core
    
    * removed import that was added by mistake

--- app/services/incremental-rollout.ts
+++ app/services/incremental-rollout.ts
@@ -116,8 +116,12 @@ class IncrementalRolloutView extends ViewHandler<IIncrementalRolloutServiceState
   featureIsEnabled(feature: EAvailableFeatures): boolean {
     if (Utils.isDevMode()) return true; // always show for dev mode
 
-    if (feature === EAvailableFeatures.aiHighlighter && getOS() !== OS.Windows) {
-      return false;
+    if (feature === EAvailableFeatures.aiHighlighter) {
+      if (getOS() !== OS.Windows) {
+        return false;
+      }
+      // always enable ai highlighter for all users on windows
+      return true;
     }
 
     return this.availableFeatures.indexOf(feature) > -1;
