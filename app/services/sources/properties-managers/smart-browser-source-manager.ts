f7a57745cea0eeb4ec76f739d8854b5c7a87e0e4
cmm21 <chris.m.milbourne@gmail.com>
2025-06-09 13:45:18 -0700

    connect to vision events (WIP)

--- app/services/sources/properties-managers/smart-browser-source-manager.ts
+++ app/services/sources/properties-managers/smart-browser-source-manager.ts
@@ -6,18 +6,33 @@ import { Subscription } from 'rxjs';
 export class SmartBrowserSourceManager extends PropertiesManager {
 	@Inject() private websocketService: WebsocketService;
 
-	websocketSub: Subscription;
+	private websocketSub!: Subscription;
+  private eventSource!: EventSource;
 
 	init() {
 		this.websocketSub = this.websocketService.socketEvent.subscribe(event => {
-			console.log('WebSocket event received:', event);
+			console.log('test event sent:', event);
 			this.obsSource.sendMessage({
 				message: JSON.stringify(event),
 			});
 		});
+
+		this.eventSource = new EventSource('http://localhost:8000/events');
+
+    this.eventSource.onmessage = (e: MessageEvent) => {
+      console.log('SSE event', e.data);
+      this.obsSource.sendMessage({
+        message: e.data,
+      });
+    };
 	}
 
 	destroy() {
-		this.websocketSub.unsubscribe();
+		if (this.websocketSub) {
+			this.websocketSub.unsubscribe();
+		}
+		if (this.eventSource) {
+			this.eventSource.close();
+		}
 	}
 }
