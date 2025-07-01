454168ef30f17c7de6b3669894a977ef0bcf7ab2
cmm21 <chris.m.milbourne@gmail.com>
2025-07-01 10:21:57 -0700

    optimize sse stream (wip)

--- app/services/sources/properties-managers/smart-browser-source-manager.ts
+++ app/services/sources/properties-managers/smart-browser-source-manager.ts
@@ -4,35 +4,27 @@ import { WebsocketService } from 'services/websocket';
 import { Subscription } from 'rxjs'; 
 
 export class SmartBrowserSourceManager extends PropertiesManager {
-	@Inject() private websocketService: WebsocketService;
+  @Inject() private websocketService: WebsocketService;
 
-	private websocketSub!: Subscription;
-  private eventSource!: EventSource;
+  private socketSub!: Subscription;
+  private sseSub!: Subscription;
 
-	init() {
-		this.websocketSub = this.websocketService.socketEvent.subscribe(event => {
-			console.log('test event sent:', event);
-			this.obsSource.sendMessage({
-				message: JSON.stringify(event),
-			});
-		});
+  init() {
+    this.socketSub = this.websocketService.socketEvent.subscribe(event => {
+      console.log('WS event', event);
+      this.obsSource.sendMessage({ message: JSON.stringify(event) });
+    });
 
-		this.eventSource = new EventSource('http://localhost:8000/events');
+    this.websocketService.openSSEConnection();
+    this.sseSub = this.websocketService.sseEvent.subscribe(e => {
+      this.obsSource.sendMessage({ message: e.data });
+    });
+  }
 
-    this.eventSource.onmessage = (e: MessageEvent) => {
-      console.log('SSE event', e.data);
-      this.obsSource.sendMessage({
-        message: e.data,
-      });
-    };
-	}
-
-	destroy() {
-		if (this.websocketSub) {
-			this.websocketSub.unsubscribe();
-		}
-		if (this.eventSource) {
-			this.eventSource.close();
-		}
-	}
+  destroy() {
+    this.socketSub?.unsubscribe();
+    this.sseSub?.unsubscribe();
+		// to completely yeet sse connections:
+    // this.websocketService.closeSSEConnection();
+  }
 }
