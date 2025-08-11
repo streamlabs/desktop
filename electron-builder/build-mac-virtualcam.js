d9852fe548f8710c1b8f753830f42c7d7ea424b0
Richard Osborne <rosborne255@yahoo.com>
2025-08-11 07:58:25 -0500

    fix codesign issue / refactor vcam code
    * relocate signApps -> build-virtual-cam
    * fix codesign issue (hardcode Streamlabs team ID into entitlements)

--- electron-builder/build-mac-virtualcam.js
+++ electron-builder/build-mac-virtualcam.js
@@ -4,8 +4,32 @@ const os = require('os');
 const pjson = require('../package.json');
 const stream = require('stream');
 
+function signApps(context) {
+  // For apps that requires specific entitlements. Ensures the entitlements file is provided during signing
+  const installer_entitlements = '--entitlements electron-builder/installer-entitlements.plist';
+  const extension_entitlements = '--entitlements electron-builder/extension-entitlements.plist';
+  const installerPath = `${context.appOutDir}/${context.packager.appInfo.productName}.app/Contents/Frameworks/slobs-virtual-cam-installer.app`;
+  const extensionPath = `${installerPath}/Contents/Library/SystemExtensions/com.streamlabs.slobs.mac-camera-extension.systemextension`;
+  const executables = [extensionPath, installerPath];
+  for (const exe of executables) {
+    const use_entitlement = exe === installerPath ? installer_entitlements : extension_entitlements;
+    console.log(`using entitlement ${use_entitlement}`);
+    cp.execSync(
+      `codesign --sign "Developer ID Application: ${context.packager.config.mac.identity}" ${use_entitlement} -o runtime --timestamp --force --verbose "${exe}"`,
+    );
+
+    // All files need to be writable for update to succeed on mac
+    console.log(`Checking Writable: ${exe}`);
+    try {
+      fs.accessSync(exe, fs.constants.W_OK);
+    } catch {
+      throw new Error(`File ${exe} is not writable!`);
+    }
+  }
+}
+
 // Download the Mac virtual camera system extension and pack it into the executable.
-async function buildVirtualCamExtension(context) {
+async function downloadVirtualCamExtension(context) {
   console.log("Download mac virtual camera");
   const destFile = 'slobs-virtual-cam-installer.tar.gz';
 
@@ -24,15 +48,14 @@ async function buildVirtualCamExtension(context) {
   await downloadFile(sourceUrl, destFile);
   console.log('Extracting tar file');
   cp.execSync(`tar -xzvf ${destFile}`);
-  // Remove the slobs developer provision profile
-  cp.execSync('rm -rf ./slobs-virtual-cam-installer.app/Contents/embedded.provisionprofile');
+  cp.execSync('rm -rf ./slobs-virtual-cam-installer.app/Contents/embedded.provisionprofile'); // remove the developer profile
+
   console.log('Copying slobs-virtual-cam-installer.app into Frameworks folder');
   cp.execSync(
     `cp -R ./slobs-virtual-cam-installer.app \"${context.appOutDir}/${context.packager.appInfo.productName}.app/Contents/Frameworks\"`,
   );
-  cp.execSync(
-    `rm -rf ./slobs-virtual-cam-installer.*`,
-  );
+
+  cp.execSync('rm -rf ./slobs-virtual-cam-installer.*');
 }
 
 function downloadFile(srcUrl, dstPath) {
@@ -59,4 +82,4 @@ function downloadFile(srcUrl, dstPath) {
   });
 }
 
-module.exports = buildVirtualCamExtension;
+module.exports = { downloadVirtualCamExtension, signApps };
