88625d8bd1b58fb89da563617308c050455a337a
AnkhHeart <yours-truly@hotmail.be>
2025-08-29 09:03:58 -0700

    lint(fix): lint this file

--- app/util/dot-tree.ts
+++ app/util/dot-tree.ts
@@ -1,15 +1,15 @@
 export function convertDotNotationToTree(states: string[] | string): any {
-    const tree: any = {};
-    const stateArray = Array.isArray(states) ? states : [states];
-    stateArray.forEach(state => {
-        const parts = state.split('.');
-        let current = tree;
-        parts.forEach((part, index) => {
-            if (!current[part]) {
-                current[part] = index === parts.length - 1 ? true : {};
-            }
-            current = current[part];
-        });
+  const tree: any = {};
+  const stateArray = Array.isArray(states) ? states : [states];
+  stateArray.forEach(state => {
+    const parts = state.split('.');
+    let current = tree;
+    parts.forEach((part, index) => {
+      if (!current[part]) {
+        current[part] = index === parts.length - 1 ? true : {};
+      }
+      current = current[part];
     });
-    return tree;
+  });
+  return tree;
 }
