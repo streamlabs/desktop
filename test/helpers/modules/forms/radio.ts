4ff92c13021958227abda22ac6a6c6af60b70830
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2025-10-06 11:02:08 -0400

    Radio button interactable in test.

--- test/helpers/modules/forms/radio.ts
+++ test/helpers/modules/forms/radio.ts
@@ -3,20 +3,20 @@ import { BaseInputController } from './base';
 export class RadioInputController extends BaseInputController<string> {
   async setValue(value: string) {
     const $el = await this.getElement();
-    $el.setValue(value);
+    const $inputs = await $el.$$('.ant-radio-input');
 
-    // const $inputs = await $el.$$('.ant-radio-input');
-    // for (const $input of $inputs) {
-    //   const inputVal = await $input.getValue();
-    //   console.log('$input', $input);
-    //   console.log('inputVal', inputVal);
-    //   if (inputVal === value) {
-    //     await $input.click();
-    //     // await $input.setValue(value);
-    //     // console.log(`Radio button with value "${value}" clicked.`);
-    //     console.log('Current value:', await this.getValue());
-    //   }
-    // }
+    for (const $input of $inputs) {
+      const inputVal = await $input.getValue();
+      const $parent = await $input.parentElement();
+      const $parentparent = await $parent.parentElement();
+
+      if (inputVal === value) {
+        await $parentparent.waitForEnabled();
+        await $parentparent.click();
+      }
+    }
+
+    $el.setValue(value);
   }
 
   async getValue() {
