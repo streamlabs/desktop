83aaf8ccf0f8bb322dca530d5f87d8482e97143c
Sean Beyer <sean.beyer@streamlabs.com>
2025-10-16 16:45:08 -0700

    Further increase timeout

--- test/helpers/modules/streaming.ts
+++ test/helpers/modules/streaming.ts
@@ -139,7 +139,7 @@ export async function stopRecording() {
 }
 
 export async function waitForSettingsWindowLoaded() {
-  return waitForEnabled('[data-name=confirmGoLiveBtn]', { timeout: 10000 });
+  return waitForEnabled('[data-name=confirmGoLiveBtn]', { timeout: 20000 });
 }
 
 export async function switchAdvancedMode() {
