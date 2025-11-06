39c679161783a8059e57b419920baf0c0eff6a8a
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2025-11-06 11:53:57 -0500

    Memoizations in Chat component.

--- app/components-react/root/Chat.tsx
+++ app/components-react/root/Chat.tsx
@@ -1,5 +1,5 @@
 import * as remote from '@electron/remote';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useCallback, useMemo } from 'react';
 import { Services } from '../service-provider';
 import styles from './Chat.m.less';
 import { OS, getOS } from '../../util/operating-systems';
@@ -15,13 +15,19 @@ export default function Chat(props: {
 
   const chatEl = useRef<HTMLDivElement>(null);
 
-  let currentPosition: IVec2 | null;
-  let currentSize: IVec2 | null;
+  const currentPosition = useRef<IVec2 | null>(null);
+  const currentSize = useRef<IVec2 | null>(null);
+  const service = useMemo(() => (props.restream ? RestreamService : ChatService), [props.restream]);
+  const windowId = useMemo(() => remote.getCurrentWindow().id, []);
 
-  let leaveFullScreenTrigger: Function;
+  const leaveFullScreenTrigger = useCallback(() => {
+    setTimeout(() => {
+      setupChat();
+      checkResize();
+    }, 1000);
+  }, []);
 
   useEffect(() => {
-    const service = props.restream ? RestreamService : ChatService;
     const cancelUnload = onUnload(() => service.actions.unmountChat(remote.getCurrentWindow().id));
 
     window.addEventListener('resize', debounce(checkResize, 100));
@@ -29,13 +35,6 @@ export default function Chat(props: {
     // Work around an electron bug on mac where chat is not interactable
     // after leaving fullscreen until chat is remounted.
     if (getOS() === OS.Mac) {
-      leaveFullScreenTrigger = () => {
-        setTimeout(() => {
-          setupChat();
-          checkResize();
-        }, 1000);
-      };
-
       remote.getCurrentWindow().on('leave-full-screen', leaveFullScreenTrigger);
     }
 
@@ -56,40 +55,36 @@ export default function Chat(props: {
   }, [props.restream]);
 
   function setupChat() {
-    const service = props.restream ? RestreamService : ChatService;
-    const windowId = remote.getCurrentWindow().id;
-
     ChatService.actions.unmountChat();
     RestreamService.actions.unmountChat(windowId);
 
     service.actions.mountChat(windowId);
-    currentPosition = null;
-    currentSize = null;
+    currentPosition.current = null;
+    currentSize.current = null;
   }
 
-  function checkResize() {
-    const service = props.restream ? RestreamService : ChatService;
-
+  const checkResize = useCallback(() => {
     if (!chatEl.current) return;
 
     const rect = chatEl.current.getBoundingClientRect();
 
-    if (currentPosition == null || currentSize == null || rectChanged(rect)) {
-      currentPosition = { x: rect.left, y: rect.top };
-      currentSize = { x: rect.width, y: rect.height };
+    if (currentPosition.current == null || currentSize == null || rectChanged(rect)) {
+      currentPosition.current = { x: rect.left, y: rect.top };
+      currentSize.current = { x: rect.width, y: rect.height };
 
-      service.actions.setChatBounds(currentPosition, currentSize);
+      service.actions.setChatBounds(currentPosition.current, currentSize.current);
     }
-  }
+  }, []);
 
-  function rectChanged(rect: ClientRect) {
+  const rectChanged = useCallback((rect: DOMRect) => {
+    if (!currentPosition.current || !currentSize.current) return;
     return (
-      rect.left !== currentPosition?.x ||
-      rect.top !== currentPosition?.y ||
-      rect.width !== currentSize?.x ||
-      rect.height !== currentSize?.y
+      rect.left !== currentPosition.current?.x ||
+      rect.top !== currentPosition.current?.y ||
+      rect.width !== currentSize.current?.x ||
+      rect.height !== currentSize.current?.y
     );
-  }
+  }, []);
 
   return <div className={styles.chat} ref={chatEl} />;
 }
