f85a7d5eb7a5e7c776141556f8e1b5f6343b444a
cmm21 <chris.m.milbourne@gmail.com>
2025-11-21 12:59:13 -0800

    reactive widget menu, add trigger settings, WIP

---
+++ app/components-react/widgets/reactive-widget/ReactiveWidgetTriggerDetails.tsx
@@ -0,0 +1,172 @@
+import React from 'react';
+import {
+  CheckboxInput,
+  MediaUrlInput,
+  NumberInput,
+  SliderInput,
+  TextInput,
+  AudioUrlInput,
+  SwitchInput,
+  FontFamilyInput,
+  ColorInput,
+  FontWeightInput,
+  FontSizeInput,
+  ListInput,
+} from 'components-react/shared/inputs';
+import { Alert, Button, Collapse, Menu, Tooltip, Row, Col } from 'antd';
+import { $t } from 'services/i18n';
+import { LayoutInput } from '../common/LayoutInput';
+import css from './ReactiveWidgetTriggerDetails.m.less';
+
+interface ReactiveWidgetTriggerDetailsProps {
+  trigger: any;
+  staticConfig?: any;
+  onUpdate?: (updatedTrigger: any) => void;
+}
+
+function flattenAnimationOptions(options: any[]): any[] {
+  const flattened: any[] = [];
+  const optionsArray = Array.isArray(options) ? options : [options];
+  optionsArray.forEach((opt) => {
+    if (opt.list && Array.isArray(opt.list)) {
+      opt.list.forEach((subOpt: any) => {
+        flattened.push({
+          label: subOpt.label,
+          value: subOpt.value,
+        });
+      });
+    } else {
+      flattened.push({
+        label: opt.label,
+        value: opt.value,
+      });
+    }
+  });
+  return flattened;
+}
+
+export function ReactiveWidgetTriggerDetails({
+  trigger,
+  onUpdate,
+  staticConfig,
+}: ReactiveWidgetTriggerDetailsProps) {
+  console.log({ trigger });
+  const isStreakTrigger = trigger?.type === 'streak';
+  const streakOptions = Object.entries(
+    staticConfig?.data?.options?.streak_time_periods ?? {},
+  ).map(([key, value]) => ({ label: String(value), value: key }));
+  const animationOptions = staticConfig?.data?.animations || [];
+  const showAnimationOptions = flattenAnimationOptions(animationOptions.show_animations);
+  const hideAnimationOptions = flattenAnimationOptions(animationOptions.hide_animations);
+  const textAnimationOptions = flattenAnimationOptions(animationOptions.text_animations);
+  const messageTemplateTooltip = $t(`When a trigger fires, this will be the format of the message. Available tokens: number`);
+  const bind = React.useCallback(
+    (path: string) => {
+      const segments = path.split('.');
+
+      let value: any = trigger;
+      for (const seg of segments) {
+        if (value == null) break;
+        value = value[seg];
+      }
+
+      return {
+        value,
+        onChange: (nextVal: any) => {
+          if (!onUpdate) return;
+          const updated: any = { ...trigger };
+          let cursor: any = updated;
+
+          for (let i = 0; i < segments.length - 1; i++) {
+            const key = segments[i];
+            cursor[key] = { ...(cursor[key] || {}) };
+            cursor = cursor[key];
+          }
+
+          cursor[segments[segments.length - 1]] = nextVal;
+          onUpdate(updated);
+        },
+      };
+    },
+    [trigger, onUpdate],
+  );
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '16px' }}>
+        <h3 style={{ marginBottom: '0', fontSize: '16px', lineHeight: '100%' }}>
+          {$t('Trigger Details')}
+        </h3>
+        <SwitchInput
+          name={`toggle-${trigger.id}`}
+          value={trigger.enabled}
+          onChange={value => onUpdate && onUpdate({ ...trigger, enabled: value as boolean })}
+          label={trigger.enabled ? $t('Enabled') : $t('Disabled')}
+          labelAlign="left"
+          layout="horizontal"
+          uncontrolled
+          checkmark
+          style={{
+            marginBottom: 0,
+            display: 'flex',
+            alignItems: 'center',
+            fontWeight: 500,
+            color: trigger.enabled ? 'var(--title)' : 'var(--nav-icon-inactive)',
+          }}
+        />
+      </div>
+      <TextInput label={$t('Name')} {...bind('name')} />
+      {isStreakTrigger && (
+        <>
+          <NumberInput label={$t('Amount Minimum')} {...bind('amount_min')} />
+          <ListInput
+            label={$t('Streak Time Period')}
+            {...bind('streak_period')}
+            options={streakOptions}
+          />
+        </>
+      )}
+      <h3 style={{ marginBottom: '16px', fontSize: '16px', lineHeight: '100%' }}>
+        {$t('Action Settings')}
+      </h3>
+      <MediaUrlInput label={$t('Media')} {...bind('media_settings.image_href')} />
+      {/* TODO$chris: if enabling custom code in the future, check here */}
+      <LayoutInput label={$t('Layout')} {...bind('layout')} />
+      <AudioUrlInput label={$t('Sound')} {...bind('media_settings.sound_href')} />
+      <SliderInput label={$t('Sound Volume')} debounce={500} {...bind('media_settings.sound_volume')} />
+      <TextInput label={$t('Message Template')} tooltip={messageTemplateTooltip} {...bind('message_template')} />
+      <SliderInput label={$t('Duration')} min={2000} max={30000} step={1} debounce={500} {...bind('alert_duration_ms')} />
+      <Collapse bordered={false}>
+        <Collapse.Panel header={$t('Font Settings')} key={1}>
+          <FontFamilyInput label={$t('Font Family')} {...bind('text_settings.font')} />
+          <FontSizeInput label={$t('Font Size')} {...bind('text_settings.font_size')} />
+          <FontWeightInput label={$t('Font Weight')} {...bind('text_settings.font_weight')} />
+          <ColorInput label={$t('Text Color')} {...bind('text_settings.font_color')} />
+          <ColorInput label={$t('Text Highlight Color')} {...bind('text_settings.font_color2')} />
+        </Collapse.Panel>
+      </Collapse>
+      <Collapse bordered={false}>
+        <Collapse.Panel header={$t('Animation Settings')} key={2}>
+          <div style={{ display: 'flex', width: '100%', alignItems: 'center', gap: '12px', marginBottom: '24px'  }}>
+            <span className={'ant-col ant-col-8 ant-form-item-label'} style={{ marginRight: '-12px', paddingRight: '10px' }}>{$t('Animation')}</span>
+            <div className={css.selectInputGroup} style={{ display: 'flex', gap: '12px', width: '100%'}}>
+              <ListInput
+                {...bind('media_settings.show_animation')}
+                options={showAnimationOptions}
+              />
+              <ListInput
+                {...bind('media_settings.hide_animation')}
+                options={hideAnimationOptions}
+              />
+            </div>
+          </div>
+          <ListInput
+            label={$t('Text Animation')}
+            {...bind('text_settings.text_animation')}
+            options={textAnimationOptions}
+          />
+          <SliderInput label={$t('Text Delay')} max={60000} {...bind('text_settings.text_delay_ms')} />
+        </Collapse.Panel>
+      </Collapse>
+    </div>
+  );
+}
