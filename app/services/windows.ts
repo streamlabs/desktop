ae8e430108b7697667e348961cf634a163e155e7
gettinToasty <sean.beyer@streamlabs.com>
2026-03-27 14:48:01 -0700

    Port Goal Widgets to React (#5761)
    * Add GenericGoal.tsx
    
    * Add widget config
    
    * Fix window rendering
    
    * Fix FontFamily input
    
    * Remove old files
    
    * Fix compile
    
    * Restore base-goal.ts for StreamBoss
    
    * Add translations
    
    * Fix tests
    
    * Fix resubs rendering
    
    * Fix goal change settings tests
    
    * Fix dropdown labels
    
    * Fix tests again
    
    * Comment out color input changes
    
    * Add proper assertions
    
    * Fix pass condition
    
    * Name form
    
    ---------
    
    Co-authored-by: gettinToasty <sbeyer@logitech.com>

--- app/services/windows.ts
+++ app/services/windows.ts
@@ -54,14 +54,6 @@ import EventFilterMenu from 'components/windows/EventFilterMenu';
 import OverlayPlaceholder from 'components/windows/OverlayPlaceholder';
 import BrowserSourceInteraction from 'components/windows/BrowserSourceInteraction';
 
-import BitGoal from 'components/widgets/goal/BitGoal';
-import DonationGoal from 'components/widgets/goal/DonationGoal';
-import SubGoal from 'components/widgets/goal/SubGoal';
-import StarsGoal from 'components/widgets/goal/StarsGoal';
-import SupporterGoal from 'components/widgets/goal/SupporterGoal';
-import SubscriberGoal from 'components/widgets/goal/SubscriberGoal';
-import FollowerGoal from 'components/widgets/goal/FollowerGoal';
-import CharityGoal from 'components/widgets/goal/CharityGoal';
 import StreamBoss from 'components/widgets/StreamBoss.vue';
 import Credits from 'components/widgets/Credits.vue';
 import TipJar from 'components/widgets/TipJar.vue';
@@ -70,7 +62,6 @@ import AlertBox from 'components/widgets/AlertBox.vue';
 import SpinWheel from 'components/widgets/SpinWheel.vue';
 import Poll from 'components/widgets/Poll';
 import ChatHighlight from 'components/widgets/ChatHighlight';
-import SuperchatGoal from 'components/widgets/goal/SuperchatGoal';
 
 import { byOS, OS } from 'util/operating-systems';
 import { UsageStatisticsService } from './usage-statistics';
@@ -113,19 +104,10 @@ export function getComponents() {
     EventFilterMenu,
     GameOverlayEventFeed,
     AdvancedStatistics,
-    BitGoal,
-    DonationGoal,
-    FollowerGoal,
-    StarsGoal,
-    SupporterGoal,
-    SubscriberGoal,
-    SuperchatGoal,
     MultistreamChatInfo,
-    CharityGoal,
     Credits,
     TipJar,
     StreamBoss,
-    SubGoal,
     MediaShare,
     AlertBox,
     SpinWheel,
