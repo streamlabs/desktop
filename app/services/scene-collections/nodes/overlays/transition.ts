6afaccd1bb88dd22fd8dc737340c6217963b500c
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2026-05-20 10:23:15 -0700

    fix: persist all transitions and connections on scene overlay export [Asana: 1208553265317640]

--- app/services/scene-collections/nodes/overlays/transition.ts
+++ app/services/scene-collections/nodes/overlays/transition.ts
@@ -1,68 +1,133 @@
 import { Node } from '../node';
 import { ETransitionType, TransitionsService } from 'services/transitions';
 import { Inject } from 'services/core/injector';
+import { TObsValue } from 'components/obs/inputs/ObsInput';
 import uniqueId from 'lodash/uniqueId';
 import path from 'path';
 import fs from 'fs';
 import { $t } from 'services/i18n';
 
-interface ISchema {
+interface ITransition {
+  id: string;
+  name: string;
   type: ETransitionType;
   duration: number;
-  settings?: Dictionary<any>;
+  settings: Dictionary<TObsValue>;
+}
+
+interface IConnection {
+  fromSceneId: string;
+  toSceneId: string;
+  transitionId: string;
+}
+
+interface ISchema {
+  transitions: ITransition[];
+  connections: IConnection[];
+  defaultTransitionId: string;
 }
 
 interface IContext {
   assetsPath: string;
 }
 
-// TODO: Fix this node
 export class TransitionNode extends Node<ISchema, IContext> {
-  schemaVersion = 1;
+  schemaVersion = 2;
 
   @Inject() transitionsService: TransitionsService;
 
   async save(context: IContext) {
-    // For overlays, we only store the default transition for now
-    const transition = this.transitionsService.getDefaultTransition();
-    const type = transition.type;
-    const settings = this.transitionsService.getSettings(transition.id);
-    const duration = transition.duration;
+    const transitions = await Promise.all(
+      this.transitionsService.state.transitions.map(async transition => {
+        const settings = { ...this.transitionsService.getSettings(transition.id) };
 
-    const filePath = settings.path as string;
+        if (transition.type === 'obs_stinger_transition' && settings.path) {
+          const filePath = settings.path as string;
+          const newFileName = `${uniqueId()}${path.parse(filePath).ext}`;
+          const destination = path.join(context.assetsPath, newFileName);
+          const input = fs.createReadStream(filePath);
+          const output = fs.createWriteStream(destination);
 
-    if (type === 'obs_stinger_transition' && filePath) {
-      const newFileName = `${uniqueId()}${path.parse(filePath).ext}`;
+          await new Promise(resolve => {
+            output.on('close', resolve);
+            input.pipe(output);
+          });
 
-      const destination = path.join(context.assetsPath, newFileName);
-      const input = fs.createReadStream(filePath);
-      const output = fs.createWriteStream(destination);
+          settings.path = newFileName;
+        }
 
-      await new Promise(resolve => {
-        output.on('close', resolve);
-        input.pipe(output);
-      });
-
-      settings.path = newFileName;
-    }
+        return {
+          id: transition.id,
+          name: transition.name,
+          type: transition.type,
+          duration: transition.duration,
+          settings,
+        };
+      }),
+    );
 
     this.data = {
-      type,
-      settings,
-      duration,
+      transitions,
+      connections: this.transitionsService.state.connections.map(connection => ({
+        fromSceneId: connection.fromSceneId,
+        toSceneId: connection.toSceneId,
+        transitionId: connection.transitionId,
+      })),
+      defaultTransitionId: this.transitionsService.state.defaultTransitionId,
     };
   }
 
   async load(context: IContext) {
     this.transitionsService.deleteAllTransitions();
 
-    if (this.data.type === 'obs_stinger_transition') {
-      this.data.settings.path = path.join(context.assetsPath, this.data.settings.path);
-    }
+    this.data.transitions.forEach(transition => {
+      const settings = { ...transition.settings };
 
-    this.transitionsService.createTransition(this.data.type, $t('Global Transition'), {
-      settings: this.data.settings,
-      duration: this.data.duration,
+      if (transition.type === 'obs_stinger_transition' && settings.path) {
+        settings.path = path.join(context.assetsPath, settings.path as string);
+      }
+
+      this.transitionsService.createTransition(transition.type, transition.name, {
+        id: transition.id,
+        settings,
+        duration: transition.duration,
+      });
     });
+
+    this.transitionsService.deleteAllConnections();
+    this.data.connections.forEach(connection => {
+      this.transitionsService.addConnection(
+        connection.fromSceneId,
+        connection.toSceneId,
+        connection.transitionId,
+      );
+    });
+
+    if (this.data.defaultTransitionId) {
+      this.transitionsService.setDefaultTransition(this.data.defaultTransitionId);
+    }
+  }
+
+  migrate(version: number) {
+    // Migrate from V1: single global transition stored at the top level
+    if (version === 1) {
+      this.data.transitions = [
+        {
+          id: null,
+          name: $t('Global Transition'),
+          // TODO: index
+          // @ts-ignore
+          type: this.data['type'],
+          // TODO: index
+          // @ts-ignore
+          duration: this.data['duration'],
+          // TODO: index
+          // @ts-ignore
+          settings: this.data['settings'] || {},
+        },
+      ];
+      this.data.connections = [];
+      this.data.defaultTransitionId = null;
+    }
   }
 }
