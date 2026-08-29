6fdccb0647183fa159e3d9ce4495d72d45587a03
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2026-08-29 01:12:45 -0700

    Revert "Add dismissable to AI Highlighter toggle in Edit Stream window. (#6128)"
    This reverts commit 5fa1b1b67cfd209dc94faff0332fa967da6741c4.

--- app/components-react/windows/go-live/platforms/TwitchEditStreamInfo.tsx
+++ app/components-react/windows/go-live/platforms/TwitchEditStreamInfo.tsx
@@ -59,9 +59,7 @@ const TwitchRequiredFields = memo((p: IPlatformComponentParams<'twitch'>) => {
         <GameSelector key="twitch-game" platform="twitch" {...bind.game} layout="vertical" />
         <TwitchTagsInput label={$t('Twitch Tags')} {...bind.tags} layout="vertical" />
       </div>
-      {p.isAiHighlighterEnabled && (
-        <AiHighlighterToggle key="ai-toggle" cardIsExpanded={false} isUpdateMode={p.isUpdateMode} />
-      )}
+      {p.isAiHighlighterEnabled && <AiHighlighterToggle key="ai-toggle" cardIsExpanded={false} />}
     </>
   );
 });
