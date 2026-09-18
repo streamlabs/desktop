f99181f60fa50165f9db16f0b06eda60edc38964
Micheline Wu <69046953+michelinewu@users.noreply.github.com>
2026-09-18 12:13:50 -0700

    Remove leftover logs from monetization implementation.

--- app/services/platforms/youtube.ts
+++ app/services/platforms/youtube.ts
@@ -1238,10 +1238,7 @@ export class YoutubeService
       this.fetchBroadcast(broadcastId),
       this.fetchVideo(broadcastId),
     ]);
-    console.log('BROADCAST');
-    console.log(JSON.stringify(broadcast, null, 2));
-    console.log('VIDEO');
-    console.log(JSON.stringify(video, null, 2));
+
     const { title, description } = broadcast.snippet;
     const { privacyStatus, selfDeclaredMadeForKids } = broadcast.status;
     const { enableDvr, projection, latencyPreference } = broadcast.contentDetails;
